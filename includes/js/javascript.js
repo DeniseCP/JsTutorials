aa86f2806e8a44d71937b639be40629a5ab6baf7
DeniseCP <decpereira@hotmail.com>
2016-10-24 21:18:34 -0700

    Signed-off-by: DeniseCP <decpereira@hotmail.com>

---
+++ includes/js/javascript.js
@@ -0,0 +1,12 @@
+function temperature(scale, unit){
+	this.scale = scale;
+	this.unit = unit;
+}
+
+function thermomether(){
+	var temperatures = initThermometer;
+}
+
+function initThermometer(array){
+	
+}
